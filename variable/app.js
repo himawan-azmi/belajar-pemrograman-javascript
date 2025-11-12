d9c43760b19ef45de48b6723f291bb8be71673f7
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-11-12 14:30:06 +0700

    update variable

---
+++ variable/app.js
@@ -0,0 +1,9 @@
+// console.log('Himawan Imtikhan Azmi');
+// console.log('Himawan Imtikhan Azmi');
+// console.log('Himawan Imtikhan Azmi');
+// console.log('Himawan Imtikhan Azmi');
+
+// Buat menjadi mudah untuk menghindari repetisi dan kemudahan pembacaan
+const myName = 'Himawan Imtikhan Azmi';
+
+console.log(myName);
