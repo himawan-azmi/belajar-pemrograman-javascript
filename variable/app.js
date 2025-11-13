02c1aad377f9cbb94df14701e8e164402c3ddb65
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-11-13 22:01:40 +0700

    Aturan Variabel

--- variable/app.js
+++ variable/app.js
@@ -7,3 +7,18 @@
 const myName = 'Himawan Imtikhan Azmi';
 
 console.log(myName);
+
+// Aturan penulisan variabel
+/*  1. Maksimal 1 kata
+    2. Tanpa symbol kecuali "_"
+    3. Tidak dimulai dengan angka
+    4. Camel Case -> myLastName
+*/
+
+const namaDepan = 'Himawan';
+const namaBelakang = 'Azmi';
+const namaLengkap = 'Himawan Imtikhan Azmi';
+
+console.log(namaDepan);
+console.log(namaBelakang);
+console.log(namaLengkap);
