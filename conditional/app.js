3b2e73e00e5dadf868d32b9a7883ef0bb78b3909
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-11-14 14:06:56 +0700

    Conditional

---
+++ conditional/app.js
@@ -0,0 +1,18 @@
+const hasSIM = true;
+
+if (hasSIM) {
+    console.log('Kamu boleh mengemudi');
+}
+
+const hasPassport = true;
+if (hasPassport) {
+    console.log('Kamu boleh ke luar negeri');
+}
+
+const isSoldOut = false;
+
+if (isSoldOut) {
+    console.log('Produk sudah habis');
+} else {
+    console.log('Produk masih tersedia');
+}
