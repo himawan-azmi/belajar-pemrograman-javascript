e814498db9a44cf5567135ba0a315e92d5536867
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-11-14 13:56:17 +0700

    Boolean

---
+++ boolean/app.js
@@ -0,0 +1,16 @@
+/* Boolean hanya terdiri dari 2 nilai
+    1. True (Benar) -> true
+    2. False (Salah) -> false
+*/
+
+const hasChildren = false;
+const isStudent = false;
+const isEmployee = true;
+const CanRideBike = true;
+const hasPet = true;
+// Tulis nama variabel dengan kata kerja
+// Is (adalah), Can (bisa), Has (memiliki)
+
+const isTall = true;
+const canCook = true;
+const hasBrother = true;
