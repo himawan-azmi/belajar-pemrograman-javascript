a90f7ffe0292a3cd4a7270213fd4a6bded7dc423
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-11-21 14:34:14 +0700

    comparison

---
+++ comparison/app.js
@@ -0,0 +1,50 @@
+if (1 == 2) {
+    console.log('Benar');
+} else {
+    console.log('Salah');
+}
+//------------------------
+console.log('======================')
+//1. 10 > 5
+if (10 > 5) {
+    console.log('Benar');
+} else {
+    console.log('Salah');
+}
+//2. 8 < 4
+if (8 < 4) {
+    console.log('Benar');
+} else {
+    console.log('Salah');
+}
+//3. -1 >= 2
+if (-1 >= 2) {
+    console.log('Benar');
+} else {
+    console.log('Salah');
+}
+//4. 0.5 <= 0
+if (0.5 <= 0) {
+    console.log('Benar');
+} else {
+    console.log('Salah');
+}
+console.log('===============================');
+// Langsung dengan console
+console.log(10 > 5);
+console.log(8 < 4);
+console.log(-1 >= 2);
+console.log(0.5 <= 0);
+console.log('==================================');
+
+const value = 'abc' === 'abc';
+console.log(value);
+
+const value1 = false !== true;
+console.log(value1);
+
+const value2 = 'cab' === 'cba';
+console.log(value2);
+
+const value3 = false === false;
+console.log(value3);
