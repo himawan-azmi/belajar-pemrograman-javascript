afe9c5e8593d8905236d43a9e64c39ad05a5a4bc
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-12-04 22:04:03 +0700

    Undefined & Null

---
+++ undefined_n_null/app.js
@@ -0,0 +1,21 @@
+// undefined digunakan untuk menyatakan suatu variabel tanpa nilai
+// undefined bisa ditemukan saat akses property di object dan elemen di array yang tidak ada
+// null berbeda dengan undefined meski sama-sama tidak ada (memiliki nilai)
+// kapan menggunakan undefined dan null?
+// lebih disarankan menggunakan null dibandingkan undefined
+
+const a = undefined;
+const b = null;
+
+console.log(a);
+console.log(b);
+console.log(a === b);
+
+const shoe = 'GeoffMax';
+const bike = 'United';
+const motorbike = null;
+const car = null;
+const boat = null;
+const airplane = null;
+
+console.log(shoe);
